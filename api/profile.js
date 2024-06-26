a47d58f27a19d7b169c72643c8cb5fbea6ae13a3
Nirav Koley <niravkoley@gmail.com>
2024-06-25 21:58:32 -0500

    implemented async cloud functions

--- api/profile.js
+++ api/profile.js
@@ -1,7 +1,7 @@
 import { getDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
 import { FIREBASE_AUTH, FIRESTORE_DB } from '../FirebaseConfig';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { fetchAsyncCloud } from './helperFuncs';
+import { fetchAsyncCloud, setAsyncCloud } from './helperFuncs';
 
 // Create the user's public data in the cloud (Firestore), store it in local storage
 export const createPublicUser = async (data) => {
@@ -24,23 +24,7 @@ export const createPublicUser = async (data) => {
 
 // Create the user's private workout data in Firestore, store it in local storage
 export const createPrivateUser = async (data) => {
-
-    
-    const user = FIREBASE_AUTH.currentUser;
-    if (user) {
-        const privateUserDataDocRef = doc(FIRESTORE_DB, 'users', user.uid, 'private', 'data');
-        try {
-            await AsyncStorage.setItem('@PrivateUserData', JSON.stringify(data));
-        } catch (e) {
-            console.log('Error saving private data to local storage: ', e)
-        }
-
-        try {
-            await setDoc(privateUserDataDocRef, data);
-        } catch (error) {
-            console.error('Error updating private data: ', error);
-        }
-    }
+    await setAsyncCloud(doc(FIRESTORE_DB, 'users', FIREBASE_AUTH.currentUser.user.uid, 'private', 'data'), '@PrivateUserData', data);
 };
 
 // Fetch the user's public data from either local storage or cloud db (Firestore)
